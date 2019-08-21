8a93accb577429f2fee07d7a64356346cd8d3043
Chiwidude <franalonzo96@gmail.com>
2019-08-21 13:18:51 -0600

    Añadiendo palabras Reservadas

---
+++ src/SINTAXIS/Token.java
@@ -0,0 +1,14 @@
+package SINTAXIS;
+
+public enum Token {
+
+        Reservadas,
+        Igual,
+        Suma,
+        Resta,
+        CADENA,
+        CONSTANTE_ENTERA,
+        CONSTANTE_DECIMAL,
+        CONSTANTE_BOOLEANA,
+        ERROR
+}
